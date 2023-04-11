a1c1a1030ed294156a71ea83cfc1a0f275e42c55
cack195 <cackamr195@gmail.com>
2023-04-12 01:49:45 +0530

    Initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,24 +1,34 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
     <div className="footer">
       <a
-        href="https://github.com/cack195"
+        href="https://twitter.com/cack195"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <FontAwesomeIcon icon={faGithubSquare} className="footer--icon" />
+        <FontAwesomeIcon icon={faTwitter} className="footer--icon" />
       </a>
+
       <a
         href="https://www.linkedin.com/in/cyril-kevin-7200161b7/"
         target="_blank"
         rel="noopener noreferrer"
       >
         <FontAwesomeIcon icon={faLinkedin} className="footer--icon" />
+      </a>
+
+      <a
+        href="https://www.instagram.com/cack195/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faInstagramSquare} className="footer--icon" />
       </a>
     </div>
   );
--- src/components/Info.js
+++ src/components/Info.js
@@ -2,24 +2,25 @@ import React from "react";
 import ProfilePhoto from "../images/Hotel_De_Ville.jpeg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 
 export default function Info() {
   return (
     <div className="info">
       <img className="info--photo" src={ProfilePhoto} alt="Cyril Kevin" />
-      <h1 className="info--fullname">Cyril Kevin</h1>
+      <h1 className="info--fullname"> Hi I'm Cyril Kevin 👋</h1>
       <h5 className="info--role">Web Developer</h5>
-      <p className="info--website">
+      {/* <p className="info--website">
         <a
           className="info--websiteLink"
           href="https://github.com/cack195"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          My Profile
+        ><u>
+          My LinkedIn Profile</u>
         </a>
-      </p>
+      </p> */}
+      <br></br>
       <div className="info--buttons">
         <address>
           <a href="mailto:cackamr195@gmail.com">
@@ -31,13 +32,13 @@ export default function Info() {
           </a>
         </address>
         <a
-          href="https://www.linkedin.com/in/cyril-kevin-7200161b7/"
+          href="https://www.github.com/cack195"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <button className="button button--linkedin">
-            <FontAwesomeIcon icon={faLinkedin} className="info--icon" />
-            LinkedIn
+          <button className="button button--github">
+            <FontAwesomeIcon icon={faGithubSquare} className="info--icon" />
+            GitHub 
           </button>
         </a>
       </div>
